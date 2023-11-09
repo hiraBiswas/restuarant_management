923c375fe0d7f451a94e968313684d10ea4e257d
hiraBiswas <hbhirabiswas@gmail.com>
2023-11-09 23:59:52 +0600

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000, // Set to a value that suits your application
+    rollupOptions: {
+      output: {
+        manualChunks: {
+         
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
